b47966d13347692e3ff5ebbb5ce6bef89ee0fadc
jaejunglee <leejj4487@gmail.com>
2019-12-08 23:28:49 +0900

    20191208 Commit

---
+++ precache-manifest.f7a71d8b5e13604a754761cf79d13010.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ff7369a15f1a447b854874a103b9d67",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "1f135ad28c492bac2fb1",
+    "url": "/movie_app/static/css/main.48ff5609.chunk.css"
+  },
+  {
+    "revision": "b4c4bae02a21e550483a",
+    "url": "/movie_app/static/js/2.9602c964.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app/static/js/2.9602c964.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1f135ad28c492bac2fb1",
+    "url": "/movie_app/static/js/main.a4dd17e7.chunk.js"
+  },
+  {
+    "revision": "c14eec7ed0651debe84c",
+    "url": "/movie_app/static/js/runtime-main.b9d7b04d.js"
+  }
+]);
